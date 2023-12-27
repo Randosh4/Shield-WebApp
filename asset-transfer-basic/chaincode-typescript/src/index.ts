9d70cabb28cb3480e2e245613b7239e2319fdd52
Rand <145016090+Randosh4@users.noreply.github.com>
2023-12-27 22:46:48 +0300

    Delete asset-transfer-basic directory

--- asset-transfer-basic/chaincode-typescript/src/index.ts
+++
@@ -1,9 +0,0 @@
-/*
- * SPDX-License-Identifier: Apache-2.0
- */
-
-import { CaseContract } from "./case.contract";
-
-export { CaseContract } from "./case.contract";
-
-export const contracts: any[] = [CaseContract];
